60c3d8f94bcdd06a8c2fbe89d0519af8d010fd3e
Dan <codetown@gmail.com>
2019-11-09 09:10:05 -0500

    Still trying to figure out typing

--- index.d.ts
+++ index.d.ts
@@ -3,6 +3,6 @@ export declare class NodeDetour {
 
   loadBin(path:string):any;
   findRandomPoint():float[];
-  findPath(float[] p1, float[] p2):float[];
-  findPath2d(float[] p1, float[] p2):float[][];
+  findPath(p1:float[], p2:float[]):float[];
+  findPath2d(p1:float[], p2:float[]):float[][];
 }
