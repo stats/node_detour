7039962254438bc1de0145d0af227a57a1c11941
Dan <codetown@gmail.com>
2019-11-09 09:27:36 -0500

    Still trying to figure out typing

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,10 @@
 declare module "node_detour" {
-  export declare class NodeDetour {
+  export class NodeDetour {
     constructor();
 
-    loadBin(path:string):any;
-    findRandomPoint():float[];
-    findPath(p1:float[], p2:float[]):float[];
-    findPath2d(p1:float[], p2:float[]):float[][];
+    public loadBin(path:string):any;
+    public findRandomPoint():float[];
+    public findPath(p1:float[], p2:float[]):float[];
+    public findPath2d(p1:float[], p2:float[]):float[][];
   }
 }
